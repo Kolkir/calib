6f3a8c6cbb4a0e7d5583c6d847f6bdc58d89e927
Kirill <kirill@c-vision.com.ua>
2013-02-26 11:17:44 +0200

    remove old build files

--- build/run_cmake.sh
+++
@@ -1,7 +0,0 @@
-mkdir ./fltk
-cd ./fltk
-cmake -DOPTION_USE_SYSTEM_LIBJPEG=FALSE -DOPTION_USE_SYSTEM_ZLIB=FALSE -DOPTION_USE_SYSTEM_LIBPNG=FALSE ../../src/external/fltk
-cd ..
-mkdir ./calib
-cd ./calib
-cmake ../../src
