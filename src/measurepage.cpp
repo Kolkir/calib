04dcde34c77d74eed3909b5f404df382d41c8703
Kirill <kirill@c-vision.com.ua>
2013-03-04 10:50:24 +0200

    measurements

--- src/measurepage.cpp
+++ src/measurepage.cpp
@@ -3,63 +3,6 @@
 
 #include <FL/Fl_File_Chooser.H>
 
-void OnMeasureChange(MeasureBox* b)
-{
-	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
-	if (ui != nullptr)
-	{
-		ui->measurePixelLength->value(b->GetMeasureLength());
-	}
-}
-
-void UpdateMeasureImage(CalibUI* ui)
-{
-	Fl_Image* img = ui->appData->measureImage.get();
-	if (img != nullptr)
-	{
-		Fl_Image* newImage = img->copy(static_cast<int>(img->w() * ui->appData->measureImageZoomValue), 
-										static_cast<int>(img->h() * ui->appData->measureImageZoomValue));
-
-		if (newImage != nullptr)
-		{
-			Fl_Image* oldImg = ui->mesureImageBox->image();
-			ui->mesureImageBox->image(newImage);
-			if (oldImg != nullptr)
-			{
-				delete oldImg;
-			}
-			ui->mesureImageBox->size(newImage->w(), newImage->h());
-			ui->mesureImageBox->SetMeasureChangeCallback(OnMeasureChange);
-			ui->mainWindow->redraw();
-		}
-	}
-}
-
-void OnLoadMeasureImage(class Fl_Button *b,void *)
-{
-	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
-	if (ui != nullptr)
-	{
-		Fl_File_Chooser fc(".", "JPG (*.{jpg,jpeg})\tPNG (*.png)\tBMP (*.bmp)", Fl_File_Chooser::MULTI, "Select image");
-		fc.show();
-		while(fc.shown())
-		{ 
-			Fl::wait(); 
-		}
-		if (fc.value() != nullptr)
-		{
-			if (fc.count() == 1)
-			{
-				char relative[FL_PATH_MAX];
-				fl_filename_relative(relative, sizeof(relative), fc.value());
-				Fl_Shared_Image* img = Fl_Shared_Image::get(relative);
-				ui->appData->measureImage.reset(img->copy());
-				UpdateMeasureImage(ui);
-			}
-		}
-	}
-}
-
 void OnMeasurePrev(class Fl_Button *b,void *)
 {
 	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
@@ -77,79 +20,33 @@ void OnMeasureCalculate(class Fl_Button *b,void *)
 		double sensorHeight = ui->sensorHeightInput->value();
 		double sensorWidth = ui->sensorWidthInput->value();
 		double focalLength = ui->focalLengthInput->value();
-		double pixelLength = ui->measurePixelLength->value();
-		double realLength = ui->measureRealLength->value();
-		double distToObj = ui->distToObjInput->value();
 
-		if (pixelLength == 0 || realLength == 0 || distToObj == 0)
+		if (ui->appData->cameraMatrix.rows != 2 &&
+			ui->appData->cameraMatrix.cols != 2)
 		{
-			fl_alert("You have to enter pixel length, real length and distance to the object");
+			fl_alert("There is no calibration data");
 		}
 		else
 		{
+			double fx = ui->appData->cameraMatrix.at<double>(0, 0);
+			double fy = ui->appData->cameraMatrix.at<double>(1, 1);
+
 			if (sensorHeight != 0 && sensorWidth != 0 && focalLength == 0)
 			{
-				double pixelPerMM1 = ui->appData->measureImage->h() / sensorHeight;
-				double pixelPerMM2 = ui->appData->measureImage->w() / sensorWidth;
-				double pixelPerMM = (pixelPerMM1 + pixelPerMM2) / 2;
-				double imgLenMM = pixelLength / pixelPerMM;
-				double magnification = imgLenMM / realLength;
-				
+				focalLength = ((fx * sensorWidth) + (fy *sensorHeight)) / 2;
+				ui->focalLengthInput->value(focalLength);
 			}
 			else if (sensorHeight == 0 && sensorWidth == 0 && focalLength != 0)
 			{
-				double magnification = focalLength / (distToObj - focalLength);
-				double imgLenMM = realLength * magnification;
-				double pixelPerMM = pixelLength / imgLenMM;
-				ui->sensorHeightInput->value(ui->appData->measureImage->h() / pixelPerMM);
-				ui->sensorWidthInput->value(ui->appData->measureImage->w() / pixelPerMM);
+				sensorHeight = focalLength / fy;
+				sensorWidth = focalLength / fx;
+				ui->sensorHeightInput->value(sensorHeight);
+				ui->sensorWidthInput->value(sensorWidth);
 			}
 			else
 			{
-				fl_alert("You have to enter focal length or sensor size either!");
+				fl_alert("You have to enter a focal length or a sensor size!");
 			}
-		}
-	}
-}
-
-void OnMeasureZoomOut(class Fl_Button *b,void *)
-{
-	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
-	if (ui != nullptr)
-	{
-		ui->appData->measureImageZoomValue -= 0.05;
-		if (ui->appData->measureImageZoomValue < 0)
-		{
-			ui->appData->measureImageZoomValue = 0.1;
-		}
-		ui->mesureImageBox->SetScaleValue(ui->appData->measureImageZoomValue);
-		UpdateMeasureImage(ui);
-	}
-}
-
-void OnMeasureZoomIn(class Fl_Button *b,void *)
-{
-	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
-	if (ui != nullptr)
-	{
-		ui->appData->measureImageZoomValue += 0.05;
-		ui->mesureImageBox->SetScaleValue(ui->appData->measureImageZoomValue);
-		UpdateMeasureImage(ui);
-	}
-}
-
-void OnChangeMeasureMode(class Fl_Check_Button *b,void *)
-{
-	CalibUI* ui = reinterpret_cast<CalibUI*>(b->window()->user_data());
-	if (ui != nullptr)
-	{
-		if (b->value() == 1)
-		{
-			ui->mesureImageBox->SetMeasurerMode(MeasureBox::FIRST_MEASURER_POINT);
-		}
-		else
-		{
-			ui->mesureImageBox->SetMeasurerMode(MeasureBox::SECOND_MEASURER_POINT);
 		}
 	}
 }
